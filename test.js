40578b2ea3f75ae295961a94d9b9e89a47a00179
Dann Brothers <spatan74@mail.com>
2018-04-16 10:43:36 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,6 @@
 if() {
   https://www.w3schools.com/
+    https://play.google.com/store/apps/details?id=nl.schoolmaster.magister.mobile
+   https://office365.com
+   https://davinci.magister.net/
 }
